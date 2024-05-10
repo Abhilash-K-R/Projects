ee47d8f202de421b9160409af138506a6727f32b
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2024-05-10 23:07:02 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -16,19 +16,3 @@ def Gen_report(city):
 Gen_report(city_name)
 
 
-
-import requests
-print("\t\tWelcome to the weather forecaster !\n\n")
-print("just enter the city you want the weather report for and click on the button ! its that simple! \n\n")
-city_name = input("Enter the name of the city : ")
-print("\n\n")
-def Gen_report(city):
-    url = "https://wttr.in/{}".format(city)
-    try:
-        data = requests.get(url)
-        weather_report = data.text
-    except:
-        weather_report = "Error Occurred"
-    print(weather_report)
-
-Gen_report(city_name)

c0415fdab9d963b4a01999f184580e38c57b5649
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-08-02 17:24:43 +0530

    Update single_img_bg.py

--- remove image background/single_img_bg.py
+++ remove image background/single_img_bg.py
@@ -1,8 +1,8 @@
 from rembg import remove
 from PIL import Image
 
-input_path = "remove image background/dodge.jpeg"
-output_path = "photo_no_bg.png"
+input_path = " "  # input image path
+output_path = " "  # output image path
 
 input_image = Image.open(input_path)
 output_image = remove(input_image)
